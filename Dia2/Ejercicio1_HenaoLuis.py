0d252c0b21fc70590b0d227467e06fb9d44b034e
Luis <roronoazoro3704@gmial.com>
2024-04-10 12:58:34 -0500

    falta

---
+++ Dia2/Ejercicio1_HenaoLuis.py
@@ -0,0 +1,30 @@
+# Al inicio, el programa dará la bienvenida y explicará la naturaleza de la Secuencia de Fibonacci, donde solicitará
+# al usuario ingresar un valor entero "n", representando hasta qué término de la secuencia se generará. Aquí mostrará la
+# secuencia de Fibonacci hasta el enésimo término ingresado por el usuario. Luego, preguntará si el usuario desea continuar
+# o salir, donde si se decide salir, el programa se detendrá; de lo contrario, se repetirá el proceso.
+
+print ("Bienbenido te explicare la naturaleza de la secuencia de Fibonacci\nEn matemática, la sucesión de Fibonacci se trata de una serie infinita de números naturales que empieza con un 0 y un 1 y continúa añadiendo números que son la suma de los dos anteriores")
+
+xd=True
+while xd==True:
+    n=int(input("Ingresa un numero y te mostrare la secuecia hasta ese termino\n"))
+
+    a=0
+    b=1
+    
+    hola=True
+    for i in range(n)
+        x=0
+        c=b+a
+        if c<99999999999 :
+
+            a=b
+            b=c
+            x=x+1
+        else:
+            print("solo se escribiran hasta el numero que tenga 11 terminos")
+            
+    
+    h=input("Quieres ingresar otro numero si/no")
+    if h=="no":
+        xd=False
