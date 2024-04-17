72ddbc586adb859f4983782a850886bc1ad50729
Luis <roronoazoro3704@gmial.com>
2024-04-16 20:43:12 -0500

    termindo y documentado

--- Dia6/Ejercicio1_HenaoLuis.py
+++ Dia6/Ejercicio1_HenaoLuis.py
@@ -1,8 +1,8 @@
 
-datos_crudos = input()
-lista_datos = [int(d) for d in datos_crudos.split()]
-x=len(lista_datos)
-while x>300:
+datos_crudos = input() #primero se introducen los numeros de la lista y se leen en str
+lista_datos = [int(d) for d in datos_crudos.split()] # se pasan los datos de datos_crudos de str a int y se guarda en lista_datos
+x=len(lista_datos)#esto es para saber cuantos datos hay en la lista 
+while x>300: #si hay mas de 300 datos en la lista le dice que ingreso muchos datos y le tiene que vuelva a ingresar la lista 
     
     print("Error ingresaste muchos numeros")
     datos_crudos = input()
@@ -11,14 +11,15 @@ while x>300:
 
     
 listaResuelta= []
-for reps in lista_datos:
-    if reps not in listaResuelta:
+for reps in lista_datos:# se usa un bucle for para mirar todos los valores que hay en lista_datos
+    if reps not in listaResuelta: #si un dato no esta en la lista vacia(listaResuelta) se agrega ahi y si ya esta se ignora
         listaResuelta.append(reps)
 
-n= len(listaResuelta)
-for i in range(n-1):
+n= len(listaResuelta) #esto es para saber cuantos valores hay en listaResuelta
+for i in range(n-1):#se usa el metodo burbuja para ordenar de manera descendente 
     for j in range(n-1-i):
         if listaResuelta[j]>listaResuelta[j+1]:
             listaResuelta[j], listaResuelta[j+1]= listaResuelta[j+1], listaResuelta[j]
 
-print(listaResuelta)
+print(listaResuelta) #finalmete se escribe la listaResuelta
+#desarrollado por Luis Orlando Henao Bermon
--- Dia6/Ejercicio2_HenaoLuis.py
+++ Dia6/Ejercicio2_HenaoLuis.py
@@ -1,10 +1,17 @@
 
-datos_crudos = input()
-lista_datos = [int(d) for d in datos_crudos.split()]
-numsuma=int(input())
-for i in range(len(lista_datos)):
-    restante=numsuma-lista_datos[i]
+#!!Al ejecutar el codigo¡¡ en la primera linea se escriben los numeros de la lista separados por espacios y en la segunda linea se escribe el numero que se quiere saber con la suma de cuales se da ese numero
+
+datos_crudos = input()#primero se introducen los numeros de la lista y se leen en str
+lista_datos = [int(d) for d in datos_crudos.split()] # se pasan los datos de datos_crudos de str a int y se guarda en lista_datos
+numsuma=int(input()) #esto es para leer el nummero objetivo que es al sumar dos de la lista
+posicion1= []#se crean listas vacias para guardar todas las opciones posibles que logren el objetivo
+posicion2=[]
+for i in range(len(lista_datos)): #se usan 2 bucles for para que por cada numero haga la suma con todos los numeros de la lista
     for a in range(len(lista_datos)):
-        if lista_datos[a]==restante:
-            print(lista_datos[a])
-           
+        if lista_datos[i]+ lista_datos[a]==numsuma: #se mira si el numero i (tomemos como ejemplo la pocicion 0) al sumarse con cada numero de la lista da como resultado el objetivo
+            posicion1.append(i) #si la suma de esos numeros da el objetivo se va guardando en en las lista posicion 1 y posicion 2
+            posicion2.append(a)
+            
+            break
+    
+print("[",posicion1[0],",", posicion2[0],"]") #muestras las pociciones de la primera opcion para llegar al objetivo 
