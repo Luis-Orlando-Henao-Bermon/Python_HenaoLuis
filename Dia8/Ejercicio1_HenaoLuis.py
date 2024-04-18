54392ba4c831eb79c74752b1feb5ca8a9b66ff1c
Luis <luismiguelcaicedobermon@gmail.com>
2024-04-18 15:53:27 -0500

    Ejercicio 1 Terminado

--- Dia8/Ejercicio1_HenaoLuis.py
+++ Dia8/Ejercicio1_HenaoLuis.py
@@ -1,6 +1,6 @@
 from os import system
 def menu():
-    print("----------Menu----------\n1. Mostrar saldo de la cuenta\n2. Recargar cuenta\n3. Comprar suscripcion\n4. Comprar suscripcion para regalar\n5. Mostrar suscripciones\n6. Salir del programa")
+    print("----------Menu----------\nPrecio de la subscripcion:50\n1. Mostrar saldo de la cuenta\n2. Recargar saldo de la cuenta\n3. Comprar suscripcion\n4. Comprar suscripcion para regalar\n5. Mostrar suscripciones\n6. Salir del programa")
 
 menu()
 opcion=int(input("Elige una opcion\n"))
@@ -8,7 +8,8 @@ precioSuscripcion=50
 saldoCuenta=0
 
 
-añosub={}
+usuarioSub={}
+usuarioSubRegalo={}
 
 bol=True
 while bol==True:
@@ -41,28 +42,89 @@ while bol==True:
             saldoCuenta=saldoCuenta-cantidadSuscripcion*precioSuscripcion
             nombreSuscripcion=(input("Ingrese su nombre\n"))
             if cantidadSuscripcion>1:
+
                 añoSuscripcion= []
+
                 for i in range(cantidadSuscripcion):
                     print("Ingrese el año para la",i+1,"suscripcion")
+                    añosub=int(input())
                     
-                    añoSuscripcion.append(int(input()))
-                añosub[nombreSuscripcion]=añoSuscripcion
-                print(añosub)
+                    while añosub<1990 or añosub>2020:
+                        añosub=int(input("Por favor ingrese un año entre el 1990 y 2020\n"))
+
+                    añoSuscripcion.append(añosub)
+                usuarioSub[nombreSuscripcion]=añoSuscripcion
                 menu()
                 opcion=int(input("Elige una opcion\n"))
             elif cantidadSuscripcion==1:
+
                 añoSuscripcion= []
                 print("Ingrese el año para la suscripcion")
-                añoSuscripcion.append(int(input()))
-                añosub[nombreSuscripcion]=añoSuscripcion
-                print(añosub)
+                añosub=int(input())
+
+                while añosub<1990 or añosub>2020:
+                    añosub=int(input("Por favor ingrese un año entre el 1990 y 2020\n"))
+
+                añoSuscripcion.append(añosub)
+                usuarioSub[nombreSuscripcion]=añoSuscripcion
+
                 menu()
                 opcion=int(input("Elige una opcion\n"))
 
     if opcion==4:
-        print("")
+        system("cls")
+        
+        cantidadSuscripcion=int(input("¿Cuantas suscripciones deseas regalar?\n"))
+        while cantidadSuscripcion<1:
+            print("Tienes que ingresar una cantidad mayor a 1")
+            cantidadSuscripcion=int(input())
+            
+        if cantidadSuscripcion*precioSuscripcion>saldoCuenta:
+            print("Saldo de cuenta insuficiente")
+            menu()
+            opcion=int(input("Elige una opcion\n"))
+        else:
+            saldoCuenta=saldoCuenta-cantidadSuscripcion*precioSuscripcion
+            nombreSuscripcion=(input("Ingrese el nombre de la persona a la que se lo quieres regalar\n"))
+            if cantidadSuscripcion>1:
+                añoSuscripcion= []
+                for i in range(cantidadSuscripcion):
+                    print("Ingrese el año para la",i+1,"suscripcion")
+                    añosub=int(input())
+
+                    while añosub<1990 or añosub>2020:
+                        añosub=int(input("Por favor ingrese un año entre el 1990 y 2020\n"))
+
+                    añoSuscripcion.append(añosub)
+                usuarioSubRegalo[nombreSuscripcion]=añoSuscripcion
+                menu()
+                opcion=int(input("Elige una opcion\n"))
+
+            elif cantidadSuscripcion==1:
+
+                añoSuscripcion= []
+
+                print("Ingrese el año para la suscripcion")
+                añosub=int(input())
+
+                while añosub<1990 or añosub>2020:
+                    añosub=int(input("Por favor ingrese un año entre el 1990 y 2020\n"))
+
+                añoSuscripcion.append(añosub)
+                usuarioSubRegalo[nombreSuscripcion]=añoSuscripcion
+                menu()
+                opcion=int(input("Elige una opcion\n"))
 
     if opcion==5:
-        print("")
+        print("Subscripciones usuario\nUsuario------Años de subscripcion")
+        for a,b in usuarioSub.items():
+            print(a,b)
+        
+        print("Subscripciones de regalo\nUsuario------Años de subscripcion")
+        for a,b in usuarioSubRegalo.items():
+            print(a,b)
+        menu()
+        opcion=int(input("Elige una opcion\n"))
+
     if opcion==6:
         bol=False
